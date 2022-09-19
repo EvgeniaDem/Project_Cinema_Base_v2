15810f91231cd79ab6ad7853ff19d60e9e2c0f14
Antoninka1301 <antonina-kupalnik@yandex.ru>
2022-09-19 13:50:12 +0300

    страница коллекций: 1. CollectionMoviesResponseDtoDaoImpl - переделка запросов 2. MovieResponseDtoDaoImpl - переделка запроса

--- src/main/java/com/kata/cinema/base/service/dto/MovieResponseDtoService.java
+++ src/main/java/com/kata/cinema/base/service/dto/MovieResponseDtoService.java
@@ -1,13 +1,8 @@
 package com.kata.cinema.base.service.dto;
 
-import com.kata.cinema.base.dao.dto.CollectionMoviesResponseDtoDao;
 import com.kata.cinema.base.models.dto.PageDto;
 import com.kata.cinema.base.models.dto.response.CollectionMoviesResponseDto;
-import com.kata.cinema.base.models.dto.response.MovieResponseDto;
-import com.kata.cinema.base.models.entitys.Genre;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Map;
 
 public interface MovieResponseDtoService {
