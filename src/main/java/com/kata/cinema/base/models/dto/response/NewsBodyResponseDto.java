4f561fb1c9398e0f349108b7feb096c7e77983c1
maxustin <maxik.ustin@gmail.com>
2022-10-04 15:51:50 +0300

    tests were fixed

--- src/main/java/com/kata/cinema/base/models/dto/response/NewsBodyResponseDto.java
+++ src/main/java/com/kata/cinema/base/models/dto/response/NewsBodyResponseDto.java
@@ -5,7 +5,6 @@ import com.kata.cinema.base.models.enums.Rubric;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.persistence.Lob;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
@@ -22,7 +21,6 @@ public class NewsBodyResponseDto {
 
     private String title;
 
-    @Lob
     private String htmlBody;
 
     private Rubric rubric;
