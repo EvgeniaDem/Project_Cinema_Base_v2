3bf9f5520399287625636b16d76da965f3d3bd65
Konovalov_Oleg <oledjan2014@gmail.com>
2022-10-05 20:01:55 +0600

    Модерация комментариев №9

--- src/test/resources/data/sql/controller/adminCommentRestController/GetCommentsInit.sql
+++ src/test/resources/data/sql/controller/adminCommentRestController/GetCommentsInit.sql
@@ -6,7 +6,7 @@ delete from public.news;
 delete from public.users;
 
 INSERT INTO users(id, avatar_url, birthday, email, first_name, last_name, login, password)
-VALUES (1, 'url', '2022-08-14', 'admin@mail.ru', 'Имя1', 'Фамилия1', 'login1', '$2a$11$Ig/7qJn7s0gllAzuvFU9vuF.FBUJKiJtzJc4xJmlnUcXklbo1QVO.');
+VALUES (1, 'url1', '2022-08-14', 'admin@mail.ru', 'Имя1', 'Фамилия1', 'login1', '$2a$11$Ig/7qJn7s0gllAzuvFU9vuF.FBUJKiJtzJc4xJmlnUcXklbo1QVO.');
 
 INSERT INTO roles(id, name)
 VALUES (1, 'ADMIN');
