06f9b633cd859b112fd0d8d5a5238df16867e0d5
Rustam Kadimov <kadim_r@mail.ru>
2022-10-06 16:01:50 +0500

    доработка комментариев к новости 11

--- src/main/java/com/kata/cinema/base/service/entity/impl/RatingCommentServiceImpl.java
+++ src/main/java/com/kata/cinema/base/service/entity/impl/RatingCommentServiceImpl.java
@@ -3,6 +3,8 @@ package com.kata.cinema.base.service.entity.impl;
 import com.kata.cinema.base.dao.entity.RatingCommentDao;
 import com.kata.cinema.base.models.dto.response.RatingCommentResponseDto;
 import com.kata.cinema.base.models.entitys.RatingComment;
+import com.kata.cinema.base.models.entitys.User;
+import com.kata.cinema.base.models.enums.TypeRating;
 import com.kata.cinema.base.service.entity.AbstractServiceImpl;
 import com.kata.cinema.base.service.entity.RatingCommentService;
 import org.springframework.stereotype.Service;
@@ -22,5 +24,10 @@ public class RatingCommentServiceImpl extends AbstractServiceImpl<Long, RatingCo
     @Override
     public List<RatingCommentResponseDto> getRatingByCommentIdAndUserId(Long commentId, Long userId) {
         return ratingCommentDao.getRatingByCommentIdAndUserId(commentId, userId);
+    }
+
+    @Override
+    public void patchComment(Long idComment, User user, TypeRating typeRating) {
+        ratingCommentDao.patchComment(idComment, user, typeRating);
     }
 }
--- src/main/java/com/kata/cinema/base/webapp/controllers/user/UserCommentRestController.java
+++ src/main/java/com/kata/cinema/base/webapp/controllers/user/UserCommentRestController.java
@@ -1,12 +1,7 @@
 package com.kata.cinema.base.webapp.controllers.user;
 
-import com.kata.cinema.base.exceptions.NotFoundByIdException;
-import com.kata.cinema.base.models.dto.response.RatingCommentResponseDto;
-import com.kata.cinema.base.models.entitys.Comment;
-import com.kata.cinema.base.models.entitys.RatingComment;
 import com.kata.cinema.base.models.entitys.User;
 import com.kata.cinema.base.models.enums.TypeRating;
-import com.kata.cinema.base.service.dto.CommentDtoService;
 import com.kata.cinema.base.service.entity.RatingCommentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,15 +10,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
-
 @RestController
 @RequestMapping("/api/user/comment")
 @AllArgsConstructor
 public class UserCommentRestController {
 
-    private final CommentDtoService commentDtoService;
     private final RatingCommentService ratingCommentService;
 
     @PatchMapping("/{id}")
@@ -37,40 +28,8 @@ public class UserCommentRestController {
             return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
         }
 
-        updateRatingEntity(id, user, typeRating);
+        ratingCommentService.patchComment(id, user, typeRating);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
-
-    private void updateRatingEntity(Long id, User user, TypeRating typeRating) {
-
-        Long userId = user.getId();
-        Optional<Comment> commentOptional = commentDtoService.getById(id);
-        if (commentOptional.isEmpty()) {
-            throw new NotFoundByIdException("You entered incorrect data, try again or contact support");
-        }
-        List<RatingCommentResponseDto> ratingCommentResponseDto = ratingCommentService.getRatingByCommentIdAndUserId(id, userId);
-
-        RatingComment ratingComment;
-
-        if (ratingCommentResponseDto.size() == 0) {
-            ratingComment = new RatingComment();
-            ratingComment.setComment(commentOptional.get());
-            ratingComment.setUser(user);
-            ratingComment.setRating(typeRating);
-            ratingCommentService.create(ratingComment);
-        } else {
-            Long ratingId = ratingCommentResponseDto.get(0).getId();
-            Optional<RatingComment> ratingCommentOptional = ratingCommentService.getById(ratingId);
-            if (ratingCommentOptional.isEmpty()) {
-                throw new NotFoundByIdException("There is no rating with ID: " + id + " , try again.");
-            }
-            if (!ratingCommentOptional.get().getRating().equals(typeRating)) {
-                ratingComment = ratingCommentOptional.get();
-                ratingComment.setRating(typeRating);
-                ratingCommentService.update(ratingComment);
-            }
-        }
-    }
-
 }
